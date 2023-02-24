31154ed223554669f3257f46df22913d0f9e5fd1
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-02-23 20:37:06 -0600

    Update main.py
    Created functions for choices made by the user as well as taking screenshots to find a pixel color. 
    Added a second option for when steam asks for mobile verification. It goes through and puts every item on the market and then goes to the next 25 items.
    Resolution function was made on a whim, I have not tested it, however from what I understand it will NOT work since I move the mouse around by exact pixel coordinates and they would be different per resolution.

--- main.py
+++ main.py
@@ -7,8 +7,66 @@ import time # for adding delays
 import pyperclip # for copying and pasting text
 from pynput.mouse import Button, Controller as MouseController # for highlighting text using mouse
 from pynput.keyboard import Key, Controller as KeyboardController # for simulating key presses
+from datetime import datetime
 
+def chooseOption():
+    option = int(input("Option 1: Go to inventory and start on whatever page you would like.\nOption 2: Specifically select if you get asked for steam mobile confirmation.\nSelect(1 or 2): "))
+    if option == 2:
+        print("Before starting confirm that you have the marketable filter selected in the inventory.")
+        return option
+    elif option == 1:
+        return option
+    else:
+        print("Option not available, try again.")
+        chooseOption()
+
+# asks user for their monitors resolution
+# NOTE THIS WILL NOT WORK EXCEPT FOR THE SECOND OPTION
+def resolutionOption():
+    resolution = int(input("Option 1: 1080p\nOption 2: 2k (2560)\nOption 3: 4k (3840)\nSelect your monitor resolution: "))
+    if resolution == 1:
+        resolution_x = 1920
+        resolution_y = 1080
+        return resolution_x, resolution_y
+    elif resolution == 2:
+        resolution_x = 2560
+        resolution_y = 1440
+        return resolution_x, resolution_y
+    elif resolution == 3:
+        resolution_x = 3840
+        resolution_y = 2160
+        return resolution_x, resolution_y
+    else:
+        print("Option not available, try again.")
+        resolutionOption()
+
+def find_pixel(color, x_start, y_start, x_end, y_end, scroll_amount, resolution_x, resolution_y):
+    while True:
+        screenshot = ImageGrab.grab(bbox=(0, 0, resolution_x, resolution_y)) # take a screenshot
+        found = False
+        for y in range(y_start, y_end):
+            for x in range(x_start, x_end):
+                pixel = screenshot.getpixel((x, y))
+                if pixel == color: # if the color is found
+                    print(f"Pixel with color {color} found at ({x}, {y})")
+                    mouse.move(x, y) 
+                    found = True
+                    break
+            if found:
+                break
+        if found:
+            return x, y
+        else:
+            # scroll by the specified amount
+            win32api.mouse_event(0x0800, 0, 0, scroll_amount, 0) # simulate scrolling
+            time.sleep(0.5)
+
+# variables
 items = []
+option = chooseOption()
+resolution_x, resolution_y = resolutionOption()
+x_iterations = 0
+y_iterations = 0
 
 input("Press enter to begin the script...")
 print("Starting Script")
@@ -17,70 +75,42 @@ print("")
 # continue running the loop until the 'q' key is pressed
 while not keyboard.is_pressed('q'):
     print("(Hold q to end)")
-    
-    # loop through the screen and check if it matches the color
-    while True:
-        color = (94, 94, 94)
-        screenshot = ImageGrab.grab() # take a screenshot
-        found = False
-        for x in range(screenshot.width):
-            for y in range(screenshot.height-20):
-                pixel = screenshot.getpixel((x, y))
-                if pixel == color: # if the color is found
-                    print(f"Pixel with color {color} found at ({x}, {y})")
-                    mouse.move(x, y) 
-                    found = True
-                    break
-            if found:
-                break
 
-        if found:
-            break
-        else:
-            # scroll down by 100 pixels
-            win32api.mouse_event(0x0800, 0, 0, 100, 0) # simulate scrolling down
-            time.sleep(0.5)
-
+    x, y = find_pixel((94, 94, 94), 0, 0, 2560, 1440, 100, resolution_x, resolution_y)
     mouse = MouseController()
-    mouse.position = (x, y)
-    time.sleep(0.1)
-    mouse.click(Button.left)
-    time.sleep(0.5)
-    mouse.position = (x - 110, y + 65)
-    time.sleep(0.1)
-    mouse.click(Button.left)
-    time.sleep(0.1)
-    mouse.position = (x - 700, y + 300)
+    mouse.position = ((x - 700) + (100 * x_iterations), (y + 300) + (100 * y_iterations))
     time.sleep(0.2)
     mouse.click(Button.left)
     time.sleep(0.5)
     
-    while True:
-        color = (94, 133, 43)
-        screenshot = ImageGrab.grab() # take a screenshot
-        found = False
-        for x in range(screenshot.width):
-            for y in range(screenshot.height):
-                pixel = screenshot.getpixel((x, y))
-                if pixel == color: # if the color is found
-                    print(f"Pixel with color {color} found at ({x}, {y})")
-                    mouse.move(x, y) 
-                    found = True
-                    break
-            if found:
-                break
+    if option == 1:
+        # loop through the screen and check if it matches the color
+        x, y = find_pixel((94, 94, 94), 0, 0, 2560, 1440, 100, resolution_x, resolution_y)
 
-        if found:
-            break
-        else:
-            # scroll down by 100 pixels
-            win32api.mouse_event(0x0800, 0, 0, -100, 0) # simulate scrolling down
-            time.sleep(0.5)
+        mouse = MouseController()
+        mouse.position = (x, y)
+        time.sleep(0.1)
+        mouse.click(Button.left)
+        time.sleep(0.5)
+        mouse.position = (x - 115, y + 65)
+        time.sleep(0.1)
+        mouse.click(Button.left)
+        time.sleep(0.1)
+        mouse.position = (x - 700, y + 300)
+        time.sleep(0.2)
+        mouse.click(Button.left)
+        time.sleep(0.5)
+        
+        x, y = find_pixel((94, 133, 43), 0, 0, 2560, 1420, -100, resolution_x, resolution_y)
+    else:
+        x, y = find_pixel((94, 133, 43), 0, 0, 2560, 1420, -100, resolution_x, resolution_y)
+        mouse = MouseController()
+        mouse.position = (x, y)
 
     # highlight the text by dragging the mouse
-    
     mouse.position = (x + 85, y - 65) 
-    mouse.press(Button.left) 
+    mouse.press(Button.left)
+    
     # move the mouse to the right in small steps
     for i in range(40):
         mouse.move(1, 0) 
@@ -158,17 +188,45 @@ while not keyboard.is_pressed('q'):
     time.sleep(0.1)
     mouse.click(Button.left)
 
+    # steam mobile authentication popup
+    if option == 2:
+        time.sleep(0.5)
+        mouse.position = (1900, 770)
+        time.sleep(0.5)
+        mouse.click(Button.left)
+        print("Steam Mobile Verification Required")
+
     # print out the item and its price
     print(f"Now selling {item} for {price}")
     print("")
     items.append({"item": item, "price": price})
     time.sleep(3)
+    x_iterations += 1
+    if x_iterations == 5:
+        x_iterations = 0
+        y_iterations += 1
+    if y_iterations == 5:
+        y_iterations = 0
+        x, y = find_pixel((94, 94, 94), 0, 0, 2560, 1440, 100, resolution_x, resolution_y)
+        mouse.position = (x - 250 , y + 775)
+        time.sleep(0.5)
+        mouse.click(Button.left)
+        time.sleep(2)
 
 print("")
 print("List of all items and prices.")
 time.sleep(2)
-num = 0
-for item in items:
-    num += 1
-    print(f"{num}. {item['item']} is being sold for {item['price']}")
+
+# Get the current date and time and format it as string
+now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+# prints results in command prompt
+# creates log file including the current time
+with open(f"output_{now}.txt", 'w') as file:
+    num = 0
+    for item in items:
+        num += 1
+        output = f"{num}. {item['item']} is being sold for {item['price']}\n"
+        print(output)
+        file.write(output)
+
 input("Press enter to exit...")
